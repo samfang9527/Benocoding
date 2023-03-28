c5d1af7de9e71bf7f49739033211b9d52f50783c
SamFang <c.s.fangyolk@gmail.com>
2023-03-28 09:20:05 +0800

    serve static

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = process.env.MAIN_SERVER_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
