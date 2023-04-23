4d2cd624b4540e5722d3c77b2de128dc217aad22
SamFang <c.s.fangyolk@gmail.com>
2023-04-23 18:03:53 +0800

    Change to production redis

--- utils/cache.js
+++ utils/cache.js
@@ -6,37 +6,37 @@ import { createAdapter } from "@socket.io/redis-adapter";
 dotenv.config();
 
 // local test
-const {
-    LOCAL_REDIS_PORT,
-    LOCAL_REDIS_HOST,
-    LOCAL_REDIS_USER,
-    LOCAL_REDIS_PWD
-} = process.env
+// const {
+//     LOCAL_REDIS_PORT,
+//     LOCAL_REDIS_HOST,
+//     LOCAL_REDIS_USER,
+//     LOCAL_REDIS_PWD
+// } = process.env
 
 // aws
-// const {
-//     ELASTICACHED_PORT,
-//     ELASTICACHED_HOST,
-//     ELASTICACHED_USER,
-//     ELASTICACHED_PWD
-// } = process.env
+const {
+    ELASTICACHED_PORT,
+    ELASTICACHED_HOST,
+    ELASTICACHED_USER,
+    ELASTICACHED_PWD
+} = process.env
 
 export function initialRedisPubSub(io) {
 
-    // const redisPub = new Redis({
-    //     port: ELASTICACHED_PORT,
-    //     host: ELASTICACHED_HOST,
-    //     username: ELASTICACHED_USER,
-    //     password: ELASTICACHED_PWD,
-    //     tls: {}
-    // });
-
     const redisPub = new Redis({
-        port: LOCAL_REDIS_PORT,
-        host: LOCAL_REDIS_HOST,
-        username: LOCAL_REDIS_USER,
-        password: LOCAL_REDIS_PWD,
+        port: ELASTICACHED_PORT,
+        host: ELASTICACHED_HOST,
+        username: ELASTICACHED_USER,
+        password: ELASTICACHED_PWD,
+        tls: {}
     });
+
+    // const redisPub = new Redis({
+    //     port: LOCAL_REDIS_PORT,
+    //     host: LOCAL_REDIS_HOST,
+    //     username: LOCAL_REDIS_USER,
+    //     password: LOCAL_REDIS_PWD,
+    // });
 
     const redisSub = redisPub.duplicate();
     io.adapter(createAdapter(redisPub, redisSub));
